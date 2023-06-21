c412ee0bcdd80477c514ee8374f8c0273f3ec456
poznerrr <pozner@gmail.com>
2023-06-21 16:22:34 +0300

    feat:connect with frontend

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\\Http\\Controllers\\Main'], function () {
+Route::get('/', 'IndexController');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
