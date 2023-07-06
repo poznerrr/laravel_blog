764a8979cdcb2f6e3c977b319be3a6b24c047be4
poznerrr <pozner@gmail.com>
2023-07-06 15:47:45 +0300

    feat: work with job

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
+use App\Jobs\StoreUserJob;
 use App\Mail\User\PasswordMail;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -15,10 +16,9 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $password = $data['password'];
-        $data['password'] = Hash::make($data['password']);
-        User::firstOrCreate(['email' => $data['email']], $data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
+
+        StoreUserJob::dispatch($data);
+
         return redirect()->route('admin.user.index');
     }
 }
---
+++ app/Jobs/StoreUserJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\User\PasswordMail;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+
+class StoreUserJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private array $data;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        //
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $password = $this->data['password'];
+        $this->data['password'] = Hash::make($this->data['password']);
+        User::firstOrCreate(['email' => $this->data['email']], $this->data);
+        Mail::to($this->data['email'])->send(new PasswordMail($password));
+    }
+}
